b98ed911df22d7aed6c42680c89e65ff27f1675a
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-14 14:30:41 -0500

    made some changes to src folder

--- employee-directory/src/components/Table/Results.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Results(props) {
-    console.log(props.results);
-}
-
-export default Results;
--- src/components/EmployeeList.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Container from './Container';
-import Navbar from './Navbar';
-import Gender from './Gender';
-import Table from './Table';
-import employees from '../employees.json';
-
-
