3d90e38eab490487e40d03181860071cef67a7b0
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-13 19:44:48 -0500

    updated api url

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from './Container';
+import Navbar from './Navbar';
+import Gender from './Gender';
+import Table from './Table';
+import employees from '../employees.json';
+
+
